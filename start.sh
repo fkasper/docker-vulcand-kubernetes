8d4ed18152633fd86aa868a5341d1ab04ee2f9dd
Florian Kasper <florian@xpandmmi.com>
2015-10-27 14:07:08 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,10 +5,13 @@ echo " _   ____  __/ /________ _____  ____/ / "
 echo "| | / / / / / / ___/ __  / __ \/ __  /  "
 echo "| |/ / /_/ / / /__/ /_/ / / / / /_/ /   "
 echo "|___/\__,_/_/\___/\__,_/_/ /_/\__,_/    "
-                                       
 
+
+
+echo "Endpoint: http://$ETCD_PORT_4001_TCP_ADDR:$ETCD_PORT_4001_TCP_PORT"
+echo "SEAL: $SEAL_KEY"
 
 /go/bin/vulcand \
-  -apiInterface=0.0.0.0 \
   -sealKey=$SEAL_KEY \
-  --etcd=http://etcd.dev.docker:4001
+  --apiInterface=0.0.0.0 \
+  --etcd=http://$ETCD_PORT_4001_TCP_ADDR:$ETCD_PORT_4001_TCP_PORT
